78827cdbd891dec650518e2d83a0944cb503b113
EveOHagan <2251923o@student.gla.ac.uk>
2019-01-22 12:16:39 +0000

    chapter 6 finished

--- rango/views.py
+++ rango/views.py
@@ -1,28 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
 
-# Create your views here.
+from django.http import HttpResponse
 
 def index(request):
-    category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
-    return render(request, 'rango/index.html', context_dict)
+	category_list = Category.objects.order_by('-likes')[:5]
+	page_list = Page.objects.order_by('-views')[:5]
+	context_dict = {'categories': category_list, 'pages':page_list}
+	return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    context_dict = {'boldmessage': "Rango says here is the about page"}
-    return render(request, 'rango/about.html', context=context_dict)
-
+	context_dict = {'boldmessage': "Rango says here is the about page"}
+	return render(request, 'rango/about.html', context=context_dict)
+	
 def show_category(request, category_name_slug):
-    context_dict = {}
-    try:
-        category = Category.objects.get(slug=category_name_slug)
-        pages = Page.objects.filter(category=category)
-        context_dict['pages'] = pages
-        context_dict['category'] = category
-    except Category.DoesNotExist:
-        context_dict['category'] = None
-        context_dict['pages'] = None
-    return render(request, 'rango/category.html', context_dict)
-    
+	context_dict = {}
+	
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+		pages = Page.objects.filter(category=category)
+		context_dict['pages'] = pages
+		context_dict['category'] = category
+	except Category.DoesNotExist:
+		context_dict['category'] = None
+		context_dict['pages'] = None
+		
+	return render(request, 'rango/category.html', context_dict)
